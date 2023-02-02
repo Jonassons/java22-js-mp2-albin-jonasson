b731ba960e16d20691a843b7c11ac75e9a84221a
Jonassons <102534278+Jonassons@users.noreply.github.com>
2023-02-02 14:17:17 +0100

    Updates for game
    Gjort fel meddelande för när man inte skriver i namn;
    Gjort så det inte går att spela utan att submitta namn;
    Gjort så att det inte går att fortsätta spela när någon vunnit utan att trycka på restart knappen;
    Gjort en alert som meddelar vem som vann rundan;

--- scripts/scrtipt.js
+++ scripts/scrtipt.js
@@ -5,9 +5,39 @@ let scores = document.querySelector("#scores");
 let name = document.querySelector("#name");
 let computerScore = 0;
 let playerScore = 0;
+let gameOver = false;
+
+const form = document.getElementById("myForm");
+const nameDisplay = document.getElementById("nameDisplay");
+
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name");
+    if (!name.value) {
+        alert("Please enter name before playing!");         // ger alert ifall man inte angett något i namn rutan
+
+        return;
+
+    }
+    nameDisplay.innerHTML = "Welcome to the game " + name.value + " !";
+
+    choices.forEach(choice => {
+        choice.style.pointerEvents = "auto";    // enable choice buttons
+    });
+});
+choices.forEach(choice => {
+    choice.style.pointerEvents = "none";        //disables choice buttons
+});
+
 
 choices.forEach(choice => {
     choice.addEventListener("click", function () {
+
+        if (gameOver) return;
+
+
         let computerChoice = Math.random();
         if (computerChoice < 0.34) {
             computerChoice = "rock";
@@ -59,11 +89,21 @@ choices.forEach(choice => {
         scores.innerHTML = `${name.value} : ${playerScore} - Computer : ${computerScore}`;
         if (playerScore === 3) {
             result.innerHTML = `${name.value} wins!`;
+            playerScore = 0;
+            computerScore = 0;
+            gameOver = true;
+            alert(`${name.value} wins!` + " Press Restart button to play again!");
         } else if (computerScore === 3) {
             result.innerHTML = `Computer wins!`;
+            playerScore = 0;
+            computerScore = 0;
+            gameOver = true;
+            alert(`Computer wins!` + " Press Restart button to play again!");
         }
     });
 });
+
+
 
 restart.addEventListener("click", function () {
     location.reload();
